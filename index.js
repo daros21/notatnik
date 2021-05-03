353b00883582fa1501997f77c4cf086d03c38c5a
daros21@github.com <daros21@op.pl>
2021-05-03 13:36:17 +0200

    notatnik - start

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const { port } = require("./config");
+
+// routes
+const apiRouter = require("./routes/api");
+
+app.use("/", apiRouter);
+
+//serwer
+app.listen(port, function () {
+  console.log(`serwer slucha na http://localhost:${port}`);
+});
